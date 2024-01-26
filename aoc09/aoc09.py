0b5f781486c18e177de3cee4c6e23f5a26d93d58
brodie-brodes <m.brodie1221@gmail.com>
2024-01-26 00:40:35 -0500

    add aoc9 p2

--- aoc09/aoc09.py
+++ aoc09/aoc09.py
@@ -7,23 +7,29 @@ def get_diff_sets(sequences):
 
     return sequences
 
-def get_next_num_in_sequence(sequence):
+def get_next_nums_in_sequence(sequence):
     diff_sets = get_diff_sets([sequence])
     for idx in range(len(diff_sets) - 2, -1, -1):
         end_set1 = diff_sets[idx][-1]
         end_set2 = diff_sets[idx + 1][-1]
 
+        start_set1 = diff_sets[idx][0]
+        start_set2 = diff_sets[idx + 1][0]
+
         diff_sets[idx].append(end_set1 + end_set2)
+        diff_sets[idx] = [start_set1 - start_set2] + diff_sets[idx]
 
-    return diff_sets[0][-1]
+    return diff_sets[0][0], diff_sets[0][-1]
 
-def p1():
+def main():
     with open("input.txt") as f:
         sequences = [[int(i) for i in line.strip().split(" ")] for line in f.read().split("\n")]
 
-    next_nums = [get_next_num_in_sequence(s) for s in sequences]
+    right_end_nums = [get_next_nums_in_sequence(s)[1] for s in sequences]
+    left_end_nums = [get_next_nums_in_sequence(s)[0] for s in sequences]
 
-    print(f"Solution (p1) - {sum(next_nums)}")
+    print(f"Solution (p1) - {sum(right_end_nums)}")
+    print(f"Solution (p2) - {sum(left_end_nums)}")
 
 if __name__ == "__main__":
-    p1()
+    main()
